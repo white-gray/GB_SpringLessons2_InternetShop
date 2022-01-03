886a3a1f41daa966d0d39d418ac9945487f8ab10
WhiteGray <WhiteGray-Black@yandex.ru>
2022-01-03 15:20:52 +0300

    main

---
+++ src/main/java/com/geekbrains/spring/web/dto/OrderItemDto.java
@@ -0,0 +1,28 @@
+package com.geekbrains.spring.web.dto;
+
+import com.geekbrains.spring.web.entities.Product;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class OrderItemDto {
+    private Long productId;
+    private String productTitle;
+    private int quantity;
+    private int pricePerProduct;
+    private int price;
+
+    public OrderItemDto(Product product) {
+        this.productId = product.getId();
+        this.productTitle = product.getTitle();
+        this.quantity = 1;
+        this.pricePerProduct = product.getPrice();
+        this.price = product.getPrice();
+    }
+
+    public void changeQuantity(int delta) {
+        this.quantity += delta;
+        this.price = this.quantity * this.pricePerProduct;
+    }
+}
