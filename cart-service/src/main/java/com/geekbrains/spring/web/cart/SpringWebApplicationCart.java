7677ed61dd7cf9fa4ff93ce1097452690a636ade
WhiteGray <WhiteGray-Black@yandex.ru>
2022-02-01 04:13:40 +0300

    study_6

---
+++ cart-service/src/main/java/com/geekbrains/spring/web/cart/SpringWebApplicationCart.java
@@ -0,0 +1,11 @@
+package com.geekbrains.spring.web.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebApplicationCart {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebApplicationCart.class, args);
+	}
+}
